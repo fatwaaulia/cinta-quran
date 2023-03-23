3b3ff611631c7f37ce76a17cf3f1530fc6d1112a
Fatwa Aulia <fatwaaulia.fy@gmail.com>
2023-03-24 05:46:56 +0700

    ajax fetch asyc

--- src/js/surah.js
+++ src/js/surah.js
@@ -1,44 +1,25 @@
 function surah() {
-    const baseUrl = 'https://equran.id';
+    const urlAPI = 'https://equran.id';
    
-    const getSurah = () => {
-      // membuat instance dari XMLHttpRequest
-      const xhr = new XMLHttpRequest();
-   
-      //menetapkan callback jika response sukses dan error
-      xhr.onload = function () {
-        const responseJson = JSON.parse(this.responseText);
-        
-        if (responseJson.code != 200) {
-          showResponseMessage(responseJson.message);
-        } else {
-          renderAllSurah(responseJson.data);
+    const getSurah = async() => {
+        try {
+            const response = await fetch(`${urlAPI}/api/v2/surat`);
+            const responseJson = await response.json();
+            if (responseJson.code != 200) {
+              showResponseMessage(responseJson.message);
+            } else {
+                renderAllSurah(responseJson.data);
+            }
+        } catch (error) {
+            alert('Ups something error');
         }
-      };
-   
-      xhr.onerror = function () {
-        showResponseMessage();
-      };
-   
-      // Membuat GET request dan menetapkan target URL
-      xhr.open('GET', `${baseUrl}/api/v2/surat`);
-   
-      // Mengirimkan request
-      xhr.send();
     };
-
-
-    
-    
-    /*
-        jangan ubah kode di bawah ini ya!
-    */
   
     const renderAllSurah = (data) => {
-      const listSurahElement = document.querySelector('#listSurah');
-      listSurahElement.innerHTML = '';
-  
-      data.forEach(dataSurah => {
+        const listSurahElement = document.querySelector('#listSurah');
+        listSurahElement.innerHTML = '';
+    
+        data.forEach(dataSurah => {
         listSurahElement.innerHTML += `
         <div class="col-lg-3">
             <div class="card mb-3">
@@ -57,22 +38,22 @@ function surah() {
                         </tr>
                     </table>
                     <div class="mt-2">
-                      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#deskripsi${dataSurah.nomor}">
-                        Deskripsi
-                      </button>
-                      <div class="modal fade" id="deskripsi${dataSurah.nomor}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h1 class="modal-title fs-5" id="exampleModalLabel">${dataSurah.namaLatin}</h1>
-                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#deskripsi${dataSurah.nomor}">
+                            Deskripsi
+                        </button>
+                        <div class="modal fade" id="deskripsi${dataSurah.nomor}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">${dataSurah.namaLatin}</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        ${dataSurah.deskripsi}
+                                    </div>
+                                </div>
                             </div>
-                            <div class="modal-body">
-                              ${dataSurah.deskripsi}
-                            </div>
-                          </div>
                         </div>
-                      </div>
 
                     </div>
                 </div>
@@ -82,10 +63,6 @@ function surah() {
       });
     };
     getSurah();
-  
-    const showResponseMessage = (message = 'Ups something error') => {
-      alert(message);
-    };
   }
   
   export default surah;
